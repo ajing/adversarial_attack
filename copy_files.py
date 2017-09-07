2c58d52c9319684c358b0faaeb232c9c17322f84
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2017-09-07 18:49:39 +0000

    current updates

--- copy_files.py
+++ copy_files.py
@@ -6,10 +6,19 @@ DEST="/home/ubuntu/cleverhans/examples/nips17_adversarial_competition"
 META_DIR = "/home/ubuntu/adversarial_attack/metafiles"
 CONFIG_DIR = "config.csv"
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
 # organize the files based on config.csv
 all_content = open(CONFIG_DIR).readlines()
-all_content = [x.strip() for x in all_content]
+all_content = [x.strip() for x in all_content if x[0] != "#"]
 attacks = all_content[0].split(",")
 attacks_target = all_content[1].split(",")
 defenses = all_content[2].split(",")
@@ -20,7 +29,7 @@ for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]
     try:
         shutil.rmtree(folder_dir)
     except:
-        print("Folder" + folder_dir + " have already been removed.")
+        print(bcolors.WARNING + "Folder" + folder_dir + " have already been removed." + bcolors.ENDC)
 
 # copy the whole folders into the destination
 for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]:
@@ -30,10 +39,10 @@ for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]
         orig_folder = os.path.join(e_folder, e_subfolder)
         dest_folder = os.path.join(folder_dir, e_subfolder)
         if os.path.isfile(orig_folder):
-            print("Copy file:" + orig_folder + " to destination folder:" + dest_folder)
+            print(bcolors.OKBLUE + "Copy file:" + orig_folder + " to destination folder:" + dest_folder + bcolors.ENDC)
             shutil.copy2(orig_folder, dest_folder)
         elif e_subfolder in attacks + attacks_target + defenses:
-            print("Copy folder:" + orig_folder + " to destination folder:" + dest_folder)
+            print(bcolors.OKBLUE + "Copy folder:" + orig_folder + " to destination folder:" + dest_folder + bcolors.ENDC)
             shutil.copytree(orig_folder, dest_folder)
 
 # copy model and meta files into directory
@@ -54,7 +63,7 @@ for e_folder in ["sample_attacks", "sample_targeted_attacks", "sample_defenses"]
                 if mod_file in ["run_defense.sh", "run_attack.sh"]:
                     mod_dir = os.path.join(dest_sub_dir, mod_file)
                     # this is only supported by python 3
-                    print("Change file mode for:"  + mod_dir)
+                    print(bcolors.OKBLUE + "Change file mode for:"  + mod_dir + bcolors.ENDC)
                     os.chmod(mod_dir, 0o777)
 
 
