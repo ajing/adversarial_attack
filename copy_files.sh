22c9d6648af2bbaae58dc7ce1942c3b65a645dc9
Ubuntu <ubuntu@ip-10-0-0-5.us-west-2.compute.internal>
2017-09-07 15:56:30 +0000

    more merging

--- copy_files.sh
+++ copy_files.sh
@@ -1,9 +1,9 @@
 DEST="/home/ubuntu/cleverhans/examples/nips17_adversarial_competition"
 # removing existing folders
-rm $DEST/sample_attacks
-rm $DEST/sample_defenses
-rm $DEST/sample_targeted_attacks
+rm -r $DEST/sample_attacks
+rm -r $DEST/sample_defenses
+rm -r $DEST/sample_targeted_attacks
 # copy folders over
-cp sample_attacks $DEST
-cp sample_defenses $DEST
-cp sample_targeted_attacks $DEST
+cp -r sample_attacks $DEST
+cp -r sample_defenses $DEST
+cp -r sample_targeted_attacks $DEST
--- sample_attacks/sal_fgsm_cb/attack_sal_fgsm.py
+++ sample_attacks/sal_fgsm_cb/attack_sal_fgsm.py
@@ -131,12 +131,12 @@ def main(_):
     #x_input_2 = tf.placeholder(tf.float32, shape=batch_shape)
 
     model = InceptionModel(num_classes)
-    
+
     salmap = SaliencyMapMethod(model)
-    fgsm = FastGradientMethod(model)
+    #fgsm = FastGradientMethod(model)
     #x_input_2 = x_input + eps * tf.sign(tf.random_normal(batch_shape))
     #x_adv = fgsm.generate(x_input_2, eps=eps, clip_min=-1., clip_max=1.)
-    x_adv = fgsm.generate(x_input, eps=eps, clip_min=-1., clip_max=1.)
+    #x_adv = fgsm.generate(x_input, eps=eps, clip_min=-1., clip_max=1.)
     x_adv_2 = salmap.generate(x_input, clip_min=-1., clip_max=1.)
     #x_adv_cb = (x_adv + x_adv_2) / 2
 
@@ -149,10 +149,10 @@ def main(_):
 
     with tf.train.MonitoredSession(session_creator=session_creator) as sess:
       for filenames, images in load_images(FLAGS.input_dir, batch_shape):
-        adv_images = sess.run(x_adv, feed_dict={x_input: images})
+        #adv_images = sess.run(x_adv, feed_dict={x_input: images})
         adv_images2 = sess.run(x_adv_2, feed_dict={x_input: images})
-        adv_images_cb = [adv_images[i] * 0.5 + adv_images2[i] * 0.5 for i in range(len(adv_images))]
-        save_images(adv_images_cb, filenames, FLAGS.output_dir)
+        #adv_images_cb = [adv_images[i] * 0.5 + adv_images2[i] * 0.5 for i in range(len(adv_images))]
+        save_images(adv_images2, filenames, FLAGS.output_dir)
 
 
 if __name__ == '__main__':
