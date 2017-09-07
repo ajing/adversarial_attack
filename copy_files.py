6523d5c6d42c56580a973ef30bc939914e7daee9
Ubuntu <ubuntu@ip-10-0-0-12.us-west-2.compute.internal>
2017-09-07 18:19:49 +0000

    rearrange to improve efficiency

--- copy_files.py
+++ copy_files.py
@@ -4,8 +4,15 @@ import subprocess
 
 DEST="/home/ubuntu/cleverhans/examples/nips17_adversarial_competition"
 META_DIR = "/home/ubuntu/adversarial_attack/metafiles"
+CONFIG_DIR = "config.csv"
 
 
+# organize the files based on config.csv
+all_content = open(CONFIG_DIR).readlines()
+all_content = [x.strip() for x in all_content]
+attacks = all_content[0].split(",")
+attacks_target = all_content[1].split(",")
+defenses = all_content[2].split(",")
 
 # removing existing folders
 for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]:
@@ -18,22 +25,31 @@ for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]
 # copy the whole folders into the destination
 for e_folder in ["sample_attacks", "sample_defenses", "sample_targeted_attacks"]:
     folder_dir = os.path.join(DEST, e_folder)
-    shutil.copytree(e_folder, folder_dir)
+    os.makedirs(folder_dir)
+    for e_subfolder in os.listdir(e_folder):
+        orig_folder = os.path.join(e_folder, e_subfolder)
+        dest_folder = os.path.join(folder_dir, e_subfolder)
+        if os.path.isfile(orig_folder):
+            print("Copy file:" + orig_folder + " to destination folder:" + dest_folder)
+            shutil.copy2(orig_folder, dest_folder)
+        elif e_subfolder in attacks + attacks_target + defenses:
+            print("Copy folder:" + orig_folder + " to destination folder:" + dest_folder)
+            shutil.copytree(orig_folder, dest_folder)
 
 # copy model and meta files into directory
 for efile in os.listdir(META_DIR):
     efile_dir = os.path.join(META_DIR, efile)
     for e_folder in ["sample_attacks", "sample_targeted_attacks"]:
         for e_subfolder in os.listdir(os.path.join(DEST, e_folder)):
-            if not "." in e_subfolder:
+            if not os.path.isfile(e_subfolder) :
                 dest_sub_dir = os.path.join(DEST, e_folder, e_subfolder)
                 shutil.copy2(efile_dir, dest_sub_dir)
 
 # and change file permissions
 for e_folder in ["sample_attacks", "sample_targeted_attacks", "sample_defenses"]:
     for e_subfolder in os.listdir(os.path.join(DEST, e_folder)):
-        if not "." in e_subfolder:
-            dest_sub_dir = os.path.join(DEST, e_folder, e_subfolder)
+        dest_sub_dir = os.path.join(DEST, e_folder, e_subfolder)
+        if not os.path.isfile(dest_sub_dir) :
             for mod_file in os.listdir(dest_sub_dir):
                 if mod_file in ["run_defense.sh", "run_attack.sh"]:
                     mod_dir = os.path.join(dest_sub_dir, mod_file)
